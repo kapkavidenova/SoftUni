fca9888f1418eca4fbfb9228dbbffbe29f21c25d
kapka videnova <videnovakapka@gmail.com>
2021-01-05 14:59:10 +0200

    Added Programming Fundamentals exercises

---
+++ Programming-Fundamentals/BasicSyntaxConditionalStatementsLoopsLab/BackInMinutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BackInMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int newMinutes = minutes + 30;
+
+            if (newMinutes > 59)
+            {
+                hours += 1;
+                newMinutes -= 60;
+            }
+
+            if (hours > 23)
+            {
+                hours -= 24;
+            }
+            Console.WriteLine($"{hours}:{newMinutes:D2}");
+        }
+    }
+}
