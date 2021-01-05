eb27597217d390e741c0db6abef22d829321d1e8
kapka videnova <videnovakapka@gmail.com>
2021-01-05 14:59:10 +0200

    Added Programming Fundamentals exercises

---
+++ Programming-Fundamentals/BasicSyntaxConditionalStatementsLoopsLab/11MultiplicationTableTwo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11MultiplicationTableTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            do
+            {
+                int product = num * multiplier;
+                Console.WriteLine($"{num} X {multiplier} = {product}");
+                multiplier++;
+            }
+            while (multiplier <= 10);
+        }
+    }
+}
